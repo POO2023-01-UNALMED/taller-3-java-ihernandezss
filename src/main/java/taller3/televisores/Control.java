82eccc564a1703016982ceedc768a4cc73277a32
ihernandezss <118498504+ihernandezss@users.noreply.github.com>
2023-02-26 15:14:56 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -3,45 +3,44 @@ package taller3.televisores;
 public class Control {
 	private TV tv;
 
-    //métodos del tv
-    //metodo para encender el televisor
+   
     public void turnOn(){
     	tv.estado = true;
     }
-    //metodo para apagar el televisor
+    
     public void turnOff(){
         tv.estado = false;
     }
-    //metodo para saber el estado del tv
+    
     public boolean getEstado(){
         return tv.estado;
     }
-    //metodo para subir el canal con condicional
+    
     public void canalUp (){
     	if (tv.estado==true) {
     		if (tv.canal >=1 && tv.canal < 120)
     			tv.canal++;
     	}
     }
-    //metodo para bajar el canal
+    
     public void canalDown (){
     	if (tv.estado==true) {
     		if (tv.canal >1 && tv.canal <= 120)
     			++tv.canal;
     	}
     }
-    //metodo para subir volumen
+    
     public void volumenUp(){
         if (tv.volumen>=0 && tv.volumen !=8 && tv.estado==true)
             tv.volumen++;
     }
-    //metodo para bajar volumen
+    
     public void volumenDown() {
         if (tv.volumen>0 && tv.volumen <=8 && tv.estado==true)
             ++tv.volumen;
     }
 
-    //método setCanal
+    
     public void setCanal(int canal) {
     	if (tv.estado==true) {
     		if (canal>=1 && canal<=120)
@@ -49,16 +48,16 @@ public class Control {
     	}
     }
     
-    //metodo enlazar
+    
     public void enlazar(TV tv){
         this.tv = tv;
         tv.control = this;
     }
-    //método get
+   
     public TV getTv() {
         return tv;
     }
-    //método set
+    
     public TV setTv(TV tv) {
         return tv;
 
